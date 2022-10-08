8bc31be634c2ccaf684737914e296ef1f003ea51
Cathrin Truchan <cathrin.truchan@gmail.com>
2022-10-08 14:20:46 +0200

    add handler function to api

--- pages/api/users/[id].js
+++ pages/api/users/[id].js
@@ -1,3 +1,14 @@
+import {getUserById} from '../../../services/userService';
+import {deleteUser} from '../../../services/userService';
+
 export default async function handler(request, response) {
-  res.status(403).json({message: 'Error: request method not allowed.'});
+  const {id} = request.query;
+  if (request.method === 'GET') {
+    const userData = await getUserById(id);
+    return response.status(200).json(userData);
+  } else if (request.method === 'DELETE') {
+    deleteUser(id);
+    response.status(202).json({message: 'User deleted', deleteID: id});
+  } else
+    response.status(403).json({message: 'Error: request method not allowed.'});
 }
--- pages/api/users/index.js
+++ pages/api/users/index.js
@@ -1,7 +1,16 @@
+import {getAllUsers} from '../../../services/userService';
+import {createUser} from '../../../services/userService';
+
 export default async function handler(request, response) {
   if (request.method === 'GET') {
-    const data = [];
+    const data = await getAllUsers();
     return response.status(200).json(data);
+  } else if (request.method === 'POST') {
+    const newUser = JSON.parse(request.body);
+    createUser(newUser.name, newUser.gender, newUser.email);
+    return response
+      .status(201)
+      .json({message: 'User created', newUser: request.body});
   }
 
   res.status(403).json({message: 'Error: request method not allowed.'});
